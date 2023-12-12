b2c5516f76d2cffb4318d93bbc613e6a15818b9c
VK-1662004 <kvishakha22it@student.mes.ac.in>
2023-12-12 23:53:35 +0530

    Final Changes

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
+
+    // Get the screen width
+var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+// Define a threshold for screen resolution
+var resolutionThreshold = 767; // Adjust this value based on your needs
+
+// Check if the screen resolution is greater than the threshold
+if (screenWidth <= resolutionThreshold) {
+    // Apply styles for high resolution
     $(".header .container ul li").each(function(i){
 
         $(this).css({"transform": "translateX("+ (i * 30)+"px" +")"})
         
         })
+} 
+
+
+   
 });
 $(document).on("click", ".menu-btn", function(){
 
